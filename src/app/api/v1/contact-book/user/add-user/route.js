85edb7d35884269f34e08c79194455b958efa8a9
Suraj Kushwaha <Kushwahasuraj093@gmail.com>
2024-04-25 22:12:03 +0530

    add user in database

--- src/app/api/v1/contact-book/user/add-user/route.js
+++ src/app/api/v1/contact-book/user/add-user/route.js
@@ -1,8 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import connect from "@/dbConfig/dbConfig";
+import User from "@/models/user";
 
 connect();
 
 export async function POST(request) {
-  return NextResponse.json({ message: "Post call for contact", success: true });
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
+
+    //check if user already exists
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return NextResponse.json({ error: "User already exists" });
+    }
+
+    const createdUser = new User({
+      username,
+      email,
+      password,
+    });
+
+    const userSave = await createdUser.save();
+
+    return NextResponse.json({
+      message: "User created successfully.",
+      success: true,
+      userSave,
+    });
+  } catch (error) {}
 }
--- src/models/user.model.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({}, { timestamps: true });
-
-const users = mongoose.model.users || mongoose.model("users", userSchema);
-
-export default users;
