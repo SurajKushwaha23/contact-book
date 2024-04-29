e1c89c59f036ba9eb8df9a921a8f6740b7dc1de0
Suraj Kushwaha <Kushwahasuraj093@gmail.com>
2024-04-29 20:15:42 +0530

    add redirection in index page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/",
+        destination: "/contact-book/index",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
