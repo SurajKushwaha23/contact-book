6a36df8bd187b3813f520ba79cc0064d21c4abb0
Suraj Kushwaha <Kushwahasuraj093@gmail.com>
2024-05-02 17:46:18 +0530

    Change banner size

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,15 @@ import Link from "next/link";
 export default function Index() {
   return (
     <>
-      <section className="py-24 bg-gray-100">
+      <section className="lg:py-24 py-8 bg-gray-100">
         <div className="mx-auto max-w-7xl">
           <div className="mx-auto max-w-4xl  text-center">
-            <h2 className="mt-8 text-3xl font-bold leading-tight text-black sm:text-4xl lg:mt-12 lg:text-5xl">
+            <h2 className="mt-8 text-6xl font-bold leading-tight text-black sm:text-4xl lg:mt-12 lg:text-5xl">
               Save your{" "}
               <span className="border-b-8 border-rose-700">Contact</span>{" "}
               without loosing it.
             </h2>
-            <p className="mx-auto pb-6 max-w-4xl text-md text-gray-600 md:mt-10 lg:text-xl">
+            <p className="mx-auto pb-6 max-w-4xl text-lg text-gray-600 md:mt-10 lg:text-xl">
               Contact Book is a tool used to store and organize information
               about people and organizations that an individual or a business
               may need to contact. It typically includes names, phone numbers,
@@ -20,7 +20,7 @@ export default function Index() {
 
             <Link
               href="/sign-up"
-              className="mt-8 rounded-md bg-rose-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-rose/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+              className="mt-8 rounded-md bg-rose-800 px-3 py-2 text-md font-semibold text-white shadow-sm hover:bg-rose/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
             >
               Get start today
             </Link>
